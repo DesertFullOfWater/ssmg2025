ba6e059811aca047207a9ac19d1dddfc2a19f10d
sasa <1374657342@qq.com>
2025-12-09 17:18:00 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -9,13 +9,13 @@ time.sleep(1)
 print('欢迎来到程序世界！')
 
 time.sleep(1)
+print('程序世界门派众多，功法丰富，变幻莫测。')
+
+time.sleep(1)
 print('接下来，我们将学习Pyhon编程语言的基本语法规则')
 
 time.sleep(1)
 print('如：程序语句、程序结构、数据类型转换、变量、函数、表达式、运算符。')
-
-time.sleep(1)
-print('程序世界内容丰富，变幻莫测。')
 
 time.sleep(1)
 print('在学习过程中，')
@@ -44,7 +44,13 @@ YoN = input('你准备好迎接编程世界的挑战了吗？（Yes/No）')
 if YoN == "Yes" or YoN == "yes":
     time.sleep(1)
     print('真不愧是三穗民高的勇士啊！')
+else:
     time.sleep(1)
-    print('话不多说，我们开始第一关：《与程序世界建立连接》')
+    print('深吸一口气，大胆往前冲，你可是打败三穗50%少年的勇士呢！')
     time.sleep(1)
-    print('使用输入输出函数链接程序世界')
+    print('小小基础编程，你肯定可以轻松拿下的。')
+
+time.sleep(1)
+print('话不多说，我们开始第一关：《与程序世界建立连接》')
+time.sleep(1)
+print('使用输入输出函数链接程序世界')
