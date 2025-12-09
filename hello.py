79817091d01c622fca088b32d80535932284538c
sasa <1374657342@qq.com>
2025-12-09 10:07:32 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,50 @@
 #第一课：输入、输出、注释
-print('**********三穗民高2025级同学，你好! ***********')
-print(input('请问你叫什么名字？'),'你好！欢迎来到程序世界！')
+import time
+print('********************************三穗民高2025级同学，你好! *********************************')
+
+time.sleep(1)
+print(input('请问你叫什么名字？'),'你好！')
+
+time.sleep(1)
+print('欢迎来到程序世界！')
+
+time.sleep(1)
 print('接下来，我们将学习Pyhon编程语言的基本语法规则')
+
+time.sleep(1)
 print('如：程序语句、程序结构、数据类型转换、变量、函数、表达式、运算符。')
+
+time.sleep(1)
 print('程序世界内容丰富，变幻莫测。')
+
+time.sleep(1)
 print('在学习过程中，')
-print('''程序中的英文可能让你头晕脑涨，
-程序中奇怪的符号可能让你找不着北，
-程序中''')
-input('你准备好迎接接下来的挑战了吗？')
+
+time.sleep(1)
+print('程序中满篇的英文可能让你头晕脑涨，')
+
+time.sleep(1)
+print('程序中奇怪的符号可能让你找不着北，')
+
+time.sleep(1)
+print('程序中丰富的语句可能让你不知从何看起......')
+
+time.sleep(1)
+print("但是……")
+
+time.sleep(1)
+print("熟能生巧，百炼成钢")
+
+time.sleep(1)
+print("请大家跟随莎姐的节奏，勤加练习。你一定会发现程序世界的奇妙的！")
+
+time.sleep(1)
+YoN = input('你准备好迎接编程世界的挑战了吗？（Yes/No）')
+
+if YoN == "Yes" or YoN == "yes":
+    time.sleep(1)
+    print('真不愧是三穗民高的勇士啊！')
+    time.sleep(1)
+    print('话不多说，我们开始第一关：《与程序世界建立连接》')
+    time.sleep(1)
+    print('')
